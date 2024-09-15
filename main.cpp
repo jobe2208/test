a843b4913a7a0fd7ed0fac3a428a080581fce2c0
Johannes Berglund <jobe2208@student.miun.se>
2024-09-15 15:38:11 +0200

    I left a comment in main.cpp.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ for(int i = 1; i <= 123; i++) {
        v.push_back(i);
    }
     auto k = std::accumulate(v.begin(), v.end(), 0);
+    //std::accumulate is better than for loop
     std::cout << k << std::endl;
     return 0;
 }
