e8260ac3d25201d942b1ec8847a23b2d8cd0af22
Johannes Berglund <jobe2208@student.miun.se>
2024-09-15 11:45:54 +0200

    Houston we have a problem

---
+++ Test.cpp
@@ -0,0 +1,8 @@
+//
+// Created by arche on 2024-09-15.
+//
+
+#include "Test.h"
+#include <iostream>
+#include <vector>
+#include <numeric>
---
+++ Test.h
@@ -0,0 +1,18 @@
+//
+// Created by arche on 2024-09-15.
+//
+
+#ifndef GIT_TEST_TEST_H
+#define GIT_TEST_TEST_H
+
+
+class Test {
+public:
+    Test();
+    ~Test();
+    void print();
+
+};
+
+
+#endif //GIT_TEST_TEST_H
