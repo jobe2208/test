2253669b9e639ad8a143836f4e7565c6733ee4db
Johannes Berglund <jobe2208@student.miun.se>
2024-09-15 11:37:08 +0200

    I change the context of the file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+   std::vector<int> v;
+for(int i = 1; i <= 123; i++) {
+       v.push_back(i);
+   }
+    auto k = std::accumulate(v.begin(), v.end(), 0);
+    std::cout << k << std::endl;
     return 0;
 }
